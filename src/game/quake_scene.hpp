45895e2b86d22a53d621edd69a24b667e774d998
Lucas Alber <lucasd.alber@gmail.com>
2026-08-09 12:32:23 +0300

    Increase default fog factor

--- src/game/quake_scene.hpp
+++ src/game/quake_scene.hpp
@@ -315,7 +315,7 @@ class QuakeScene : public merian::Scene {
     float mu_t = 0.0F;
     merian::float3 mu_s_div_mu_t{1};
     float fog_particle_size_um = 7.0F;
-    float fog_density_factor = 0.1F;
+    float fog_density_factor = 0.5F;
     bool reproducible_renders = false;
 
     merian::Scene::CameraID quake_camera;
